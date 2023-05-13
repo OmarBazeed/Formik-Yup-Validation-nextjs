e3a69793a4d0c1f8d0af5a25ea1ae54ebe459bb3
omar bazeed <omarbazeed@gmail.com>
2023-05-13 18:08:37 +0200

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="bg-slate-500 block text-red-400">
+        Hello Formik And Yup In Next Js project
+      </h1>
+    </main>
+  )
+}
---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>The Header</div>
+    )
+}
+
+export default Header
